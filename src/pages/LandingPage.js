f2785740122ebf0748658433a1c5b9fa9279dcdc
Daniel Niedzielski <daniel.niedziels@wp.pl>
2023-11-15 16:12:27 +0100

    Cos popsulem xD

--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -2,7 +2,7 @@ import { FaInstagram } from "react-icons/fa";
 import LoginForm from "./LoginForm";
 function LandingPage() {
   return (
-    <div className="flex flex-col mx-0 m-auto">
+    <div className="w-1/2 flex flex-col gap-1/2 items-center border border-slate-500 mx-0 m-auto">
       <FaInstagram size={"10rem"} />
       <LoginForm />
     </div>
--- src/pages/LoginForm.js
+++ src/pages/LoginForm.js
@@ -1,21 +1,40 @@
 import { useState } from "react";
-
+import 
 function LoginForm() {
+  const [userData, setUserData] = useState([]);
+
   const [inputs, setInputs] = useState({
     login: "",
     password: "",
   });
+  const [formValidity, setFormValidity] = useState("");
+
   const inputHandler = (e) => {
-    setInputs({
-      [e.target.name]: e.target.value,
-      ...inputs,
-    });
+    setInputs({ [e.target.name]: e.target.value });
   };
+
+  const formSubmitHandler = (event) => {
+    event.prefentDefault();
+    if (inputs.login.includes("@")) {
+      setFormValidity(true);
+    }
+  };
+
+  // useEffect(async () => {
+  //   const fetchedData = await fetch("");
+  //   const resp = (await fetchedData).json();
+  //   const data = setUserData(resp);
+  // });
   return (
-    <>
-      <form className="flex flex-col gap-2" action="">
+    <div className="">
+      <form
+        onSubmit={formSubmitHandler}
+        className="flex flex-col gap-2"
+        action=""
+      >
         <input
-          className="px-2 py-1"
+          onChange={inputHandler}
+          className={"px-2"}
           type="text"
           name="login"
           id=""
@@ -23,17 +42,28 @@ function LoginForm() {
           value={inputs.login}
         />
         <input
+          onChange={inputHandler}
           className="px-2 py-1"
           type="password"
           name="password"
           value={inputs.password}
           id=""
+          placeholder="password"
         />
-        <div>
-          <input type="submit" value="SIGN IN" />
+        <div className="flex gap-3">
+          <input
+            className="bg-slate-500 px-2 py-3 rounded-sm"
+            type="submit"
+            value="SIGN IN"
+          />
+          <input
+            className="bg-red-500 px-2 py-3 rounded-sm"
+            type="submit"
+            value="SIGN UP"
+          />
         </div>
       </form>
-    </>
+    </div>
   );
 }
 export default LoginForm;
