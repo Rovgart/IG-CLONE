ffb67d41c0fd9e119fd2aa7f23a7fc5f88de8449
c0rly <150666326+c0rly@users.noreply.github.com>
2023-12-27 14:18:24 +0100

    Delete src/config directory
    src

--- src/config/dbConfig.js
+++
@@ -1,9 +0,0 @@
-import { Sequelize } from "sequelize";
-
-const db = new Sequelize("ig_clone", "postgres", "test", {
-	host: "localhost",
-	dialect: "postgres",
-	operatorsAliases: false,
-});
-
-export default db;
